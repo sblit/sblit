133504ebd662526a216993a63143ef02c8a52a98
Xaeras <andreasnovak@hotmail.de>
2015-03-03 19:08:42 +0100

    Moved files into right folder

--- src/net/sblit/gui/ConfigurationDialog.java
+++ src/net/sblit/gui/ConfigurationDialog.java
@@ -1,4 +1,4 @@
-package org.sblit.gui;
+package net.sblit.gui;
 
 import java.io.File;
 import java.util.Random;
--- src/net/sblit/gui/SystemTray.java
+++ src/net/sblit/gui/SystemTray.java
@@ -1,4 +1,4 @@
-package org.sblit.gui;
+package net.sblit.gui;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
